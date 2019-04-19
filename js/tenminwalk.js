9c5549804f50b80ec8c337b4e7d7ce3dd1b3868b
Tehya Hubbert <taye@Tehyas-MacBook-Pro.local>
2019-04-19 14:51:17 -0400

    fixed formatting problem

--- js/tenminwalk.js
+++ js/tenminwalk.js
@@ -1,4 +1,8 @@
-//everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block in a direction and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+//everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']).
+//You always walk only a single block in a direction and
+//you know it takes you one minute to traverse one city block
+//create a function that will return true if the walk the app gives you will take you exactly ten minutes
+//and return you to your starting point. Return false otherwise.
 
 
 
