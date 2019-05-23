7d43c0912350df109d4b9f1a759abf52ec6f1dc7
Tehya Hubbert <taye@Tehyas-MacBook-Pro.local>
2019-05-22 21:28:47 -0400

    added another solution and fixed js syntax

--- js/zerostoend.js
+++ js/zerostoend.js
@@ -5,17 +5,22 @@ const moveZeros = function (arr) {
     //move all zeros to one array, non zeros to the other
     arr.forEach(num => {
         if (num === 0) {
-            zeros.push(num)
+            zeros.push(num);
         } else {
-            all.push(num)
-        };
+            all.push(num);
+        }
     });
     //combine arrays and return the combination
     return all.concat(zeros);
-}
+};
 
 //test code
 Test.assertEquals(
     JSON.stringify(moveZeros([1, 2, 0, 1, 0, 1, 0, 3, 0, 1])),
     JSON.stringify([1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
 );
+
+//shorter way (study it)
+//var moveZeros = function (arr) {
+//    return arr.filter(function (x) { return x !== 0 }).concat(arr.filter(function (x) { return x === 0; }));
+//}
